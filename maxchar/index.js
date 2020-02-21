bec8fa31084877be6d803d2bd372cdb5dc226a3b
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-21 14:25:46 -0600

    completed maxChar objective and chunk

---
+++ maxchar/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    let obj = {} 
+    let count = 0
+    let maxChar = ''
+    
+    str = str.split('')
+    for(let char of str){
+        if(!obj[char]){
+            obj[char] = 1
+        }
+        obj[char] += 1
+    }
+    for(let item in obj){
+        if(count < obj[item]){
+            count = obj[item]
+            maxChar = item
+        }
+        item++
+    }
+    return maxChar
+}
+
+
+module.exports = maxChar;
